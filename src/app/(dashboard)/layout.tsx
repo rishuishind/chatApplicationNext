a25e9b000d018e369530b12354a547a107856004
rishabh <hespnod2348@gmai.com>
2024-03-12 12:53:32 +0530

    made the UI and implimented logic for logging out the user

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import { Icon, Icons } from "@/components/Icons"
+import SignOutButton from "@/components/SignOutButton"
 import { authOptions } from "@/lib/auth"
 import { getServerSession } from "next-auth"
+import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
 import { ReactNode } from "react"
@@ -26,7 +28,7 @@ const sidebarOptions:SidebarOption[]=[
 
 
 const Layout = async ({children}:LayoutProps) => {
-    const session = getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
     if(!session) notFound();
   return (
     <div className="w-full flex h-screen">
@@ -55,6 +57,19 @@ const Layout = async ({children}:LayoutProps) => {
                     )
                   })}
                  </ul>
+                </li>
+                <li className="-mx-6 mt-auto flex">
+                  <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
+                    <div className="relative h-8 w-8 bg-gray-50">
+                      <Image fill referrerPolicy="no-referrer" className="rounded-full" src={session.user.image || ''} alt="user-image"/>
+                    </div>
+                    <span className="sr-only">Your Profile</span>
+                    <div className="flex flex-col">
+                      <span aria-hidden='true'>{session.user.name}</span>
+                      <span className="text-xs text-zinc-400" aria-hidden='true'>{session.user.email}</span>
+                    </div>
+                  </div>
+                  <SignOutButton className='h-full aspect-square'/>
                 </li>
               </ul>
             </nav>
--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -14,10 +14,12 @@ type FormData = z.infer<typeof addFriendValidator>
 const AddFriendButton:FC<AddFriendButtonProps> = ({}) => {
 
     const [showSuccessState,setShowSuccessState] = useState<boolean>(false);
+    const [isLoading,setIsLoading] = useState(false);
 
     const{register,handleSubmit,setError,formState:{errors}} = useForm<FormData>({resolver:zodResolver(addFriendValidator)})
 
     const addFriend=async(email:string)=>{
+        setIsLoading(true);
         try {
             const validateEmail = addFriendValidator.parse({email});
             await axios.post('/api/friends/add',{
@@ -35,6 +37,8 @@ const AddFriendButton:FC<AddFriendButtonProps> = ({}) => {
                 return;
             }
             setError('email',{message:'Something went wrong.'});
+        }finally{
+            setIsLoading(false);
         }
     }
     const onSubmit=(data:FormData)=>{
@@ -45,7 +49,7 @@ const AddFriendButton:FC<AddFriendButtonProps> = ({}) => {
         <label htmlFor="email" className='block text-sm font-medium leading-6 text-gray-900'>Add friend by E-Mail</label>
         <div className='mt-2 flex gap-4'>
             <input {...register('email')} type="text" className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='you@example.com' />
-            <Button>Add</Button>
+            <Button isLoading={isLoading}>Add</Button>
         </div>
         <p className='mt-1 text-sm text-red-600'>{errors.email?.message}</p>
         {showSuccessState && <p className='mt-1 text-sm text-green-600'>Friend request sent!</p>}
